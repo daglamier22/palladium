d4a69fbce33938993e91a5dbe32af110342a27c9
Brian McCarthy <daglamier22@gmail.com>
2019-02-03 11:40:34 -0600

    moved previous app.component logic to new homepage

--- src/app/views/homepage/homepage.component.ts
+++ src/app/views/homepage/homepage.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs/operators';
+
+import { HelloWorldService } from '../../services/helloworld.service';
 
 @Component({
   selector: 'app-homepage',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent implements OnInit {
+  message = 'hi';
+  loading: boolean;
 
-  constructor() { }
+  constructor(
+    private helloWorldService: HelloWorldService
+  ) { }
 
   ngOnInit() {
+    this.helloWorldService.call();
+    this.helloWorldService.getLoadingChanged().pipe(take(1)).subscribe(
+      (loading: boolean) => {
+        this.loading = loading;
+        this.message = this.helloWorldService.getServerResponse();
+      }
+    );
   }
-
 }
