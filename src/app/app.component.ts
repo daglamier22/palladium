38a8532438ad6b0cd28afe741d48bd772f39674e
Brian McCarthy <daglamier22@gmail.com>
2019-02-03 11:41:40 -0600

    clean up app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { take } from 'rxjs/operators';
-
-import { HelloWorldService } from './services/helloworld.service';
 
 @Component({
   selector: 'app-root',
@@ -9,18 +6,9 @@ import { HelloWorldService } from './services/helloworld.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'budget-frontend';
-  loading: boolean;
 
-  constructor(private helloWorldService: HelloWorldService) {}
+  constructor() {}
 
   ngOnInit() {
-    this.helloWorldService.call();
-    this.helloWorldService.getLoadingChanged().pipe(take(1)).subscribe(
-      (loading: boolean) => {
-        this.loading = loading;
-        this.title = this.helloWorldService.getServerResponse();
-      }
-    );
   }
 }
