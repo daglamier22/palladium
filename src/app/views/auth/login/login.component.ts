eb6c532f1e0c7de5ec8a8371f148bb377d22af07
Brian McCarthy <daglamier22@gmail.com>
2019-02-03 16:59:38 -0600

    flesh out login form

--- src/app/views/auth/login/login.component.ts
+++ src/app/views/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.loginForm = this.createFormGroup();
   }
 
+  onSubmit() {
+    this.markControlsTouched();
+    if (this.loginForm.valid) {
+      console.log('form valid');
+    } else {
+      console.log('form invalid');
+    }
+  }
+
+  createFormGroup(): FormGroup {
+    return new FormGroup({
+      username: new FormControl('', {
+        validators: [Validators.required]
+      }),
+      password: new FormControl('', {
+        validators: [Validators.required]
+      })
+    });
+  }
+
+  markControlsTouched() {
+    Object
+      .keys(this.loginForm.controls)
+      .forEach(field => {
+        const control = this.loginForm.get(field);
+        control.markAsTouched({onlySelf: true});
+      });
+  }
+
+  getFormControlErrors(controlName: string): string {
+    const control = this.loginForm.get(controlName);
+    if (control && control.touched && control.errors) {
+      if (control.errors.required) {
+        return 'This is a required field.';
+      }
+    }
+    return '';
+  }
 }
