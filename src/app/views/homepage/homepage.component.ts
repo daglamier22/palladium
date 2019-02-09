8e14f4a4d35e50b609f7566a41fe774d7d49facb
Brian McCarthy <daglamier22@gmail.com>
2019-02-09 13:52:12 -0600

    retire helloworld service

--- src/app/views/homepage/homepage.component.ts
+++ src/app/views/homepage/homepage.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { take } from 'rxjs/operators';
 
-import { HelloWorldService } from '../../services/helloworld.service';
 import { VersionService } from '../../services/version.service';
 
 @Component({
@@ -10,28 +9,20 @@ import { VersionService } from '../../services/version.service';
   styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent implements OnInit {
-  message = 'hi';
+  versionBackend: string;
+  versionFrontend: string;
   loading: boolean;
 
   constructor(
-    private helloWorldService: HelloWorldService,
     private versionService: VersionService
   ) { }
 
   ngOnInit() {
-    this.helloWorldService.call();
-    this.helloWorldService.getLoadingChanged().pipe(take(1)).subscribe(
-      (loading: boolean) => {
-        this.loading = loading;
-        this.message = this.helloWorldService.getServerResponse();
-      }
-    );
-
     this.versionService.call();
     this.versionService.getLoadingChanged().pipe(take(1)).subscribe(
       (loading: boolean) => {
         this.loading = loading;
-        this.message = this.versionService.getServerResponse();
+        this.versionBackend = this.versionService.getServerResponse();
       }
     );
   }
