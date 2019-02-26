5adbbda54695cf8e28fc7cc7164ac9987fe1b6c7
Brian McCarthy <daglamier22@gmail.com>
2019-02-25 18:43:08 -0600

    create get-account service

---
+++ src/app/services/accounts/get-accounts/get-accounts.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import * as util from '../../util';
+import { AuthService } from '../../auth/auth.service';
+import { GetAccountResponse } from './get-accounts.model';
+
+const signupURL = '/get-accounts';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GetAccountsService {
+  private loading: boolean;
+  private loadingChanged = new Subject<boolean>();
+  private serverResponse: GetAccountResponse;
+
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService
+  ) { }
+
+  getLoading(): boolean {
+    return this.loading;
+  }
+
+  getLoadingChanged(): Observable<boolean> {
+    return this.loadingChanged.asObservable();
+  }
+
+  getServerResponse(): GetAccountResponse {
+    return this.serverResponse;
+  }
+
+  call() {
+    if (this.loading) {
+      return;
+    }
+
+    this.loading = true;
+    this.loadingChanged.next(this.loading);
+
+    const fullURL = util.determineServerURL() + signupURL;
+    this.http.get<GetAccountResponse>(
+      fullURL,
+      { headers: new HttpHeaders({'Content-Type': 'application/json', 'Authorization': `Bearer ${this.authService.getToken()}`}) }
+    ).subscribe((response: GetAccountResponse) => {
+      this.serverResponse = { ...response };
+      console.log(this.serverResponse);
+      this.loading = false;
+      this.loadingChanged.next(this.loading);
+    }, (error) => {
+      console.log('GetAccountsService call: ', error);
+      this.serverResponse = {
+        message: error.error.message,
+        status: error.error.status,
+        values: {
+          accounts: []
+        }
+      };
+      this.loading = false;
+      this.loadingChanged.next(this.loading);
+    });
+  }
+}
