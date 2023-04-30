74591865ed025a9167a39fc9ad1d630b6d7fec7c
Brian McCarthy <daglamier22@gmail.com>
2023-04-30 16:04:40 -0500

    create createitem service and add it to navbar view

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgxPlaidLinkService, PlaidConfig, PlaidErrorMetadata, PlaidEventMetadata, PlaidLinkHandler, PlaidSuccessMetadata } from 'ngx-plaid-link';
 
 import { AuthService } from '../../services/auth/auth.service';
 import { CreateLinkTokenService } from '../../services/plaid/create-link-token/create-link-token.service';
-import { NgxPlaidLinkService, PlaidConfig, PlaidErrorMetadata, PlaidEventMetadata, PlaidLinkHandler, PlaidSuccessMetadata } from 'ngx-plaid-link';
+import { CreateItemService } from '../../services/plaid/create-item/create-item.service';
 
 @Component({
   selector: 'app-navbar',
@@ -20,7 +21,8 @@ export class NavbarComponent implements OnInit {
     private router: Router,
     private authService: AuthService,
     private createLinkTokenService: CreateLinkTokenService,
-    private plaidLinkService: NgxPlaidLinkService
+    private plaidLinkService: NgxPlaidLinkService,
+    private createItemService: CreateItemService
   ) { }
 
   ngOnInit() {
@@ -35,7 +37,40 @@ export class NavbarComponent implements OnInit {
   }
 
   public onAddAccount(): void {
-    this.createLinkTokenService.call();
+    if (this.plaidLinkHandler) {
+      this.plaidLinkHandler.open();
+    } else {
+      this.createLinkTokenService.call();
+      this.subscribeToCreateLinkTokenService();
+    }
+  }
+
+  private onPlaidSuccess(token: string, metadata: PlaidSuccessMetadata) {
+    // Send the public token to your server so you can do the token exchange.
+    console.log('success', token, metadata);
+    if (token) {
+      const institutionId = metadata?.institution?.institution_id ? metadata?.institution?.institution_id : '';
+      this.createItemService.call(token, institutionId);
+      this.subscribeToCreateItemService();
+    }
+  }
+
+  private onPlaidExit(error: string, metadata: PlaidErrorMetadata) {
+    // TODO: - Get errors or exit reason.
+    console.log('exit', error, metadata);
+  }
+
+  private onPlaidEvent(eventName: string, metadata: PlaidEventMetadata) {
+    // TODO: - Log events so you can have insight into how your users are using plaid link.
+    console.log('event', eventName, metadata);
+  }
+
+  private onPlaidLoad(event: string) {
+    // TODO: - Do something when the iframe loads.
+    console.log('load', event);
+  }
+
+  private subscribeToCreateLinkTokenService() {
     this.createLinkTokenService.getLoadingChanged().subscribe((loading: boolean) => {
       let linkToken = this.createLinkTokenService.getServerResponse().values.linkToken;
       console.log(linkToken);
@@ -57,23 +92,12 @@ export class NavbarComponent implements OnInit {
       });
   }
 
-  onPlaidSuccess(token: string, metadata: PlaidSuccessMetadata) {
-    // Send the public token to your server so you can do the token exchange.
-    console.log('success', token, metadata);
-  }
-
-  onPlaidExit(error: string, metadata: PlaidErrorMetadata) {
-    // TODO: - Get errors or exit reason.
-    console.log('exit', error, metadata);
-  }
-
-  onPlaidEvent(eventName: string, metadata: PlaidEventMetadata) {
-    // TODO: - Log events so you can have insight into how your users are using plaid link.
-    console.log('event', eventName, metadata);
-  }
-
-  onPlaidLoad(event: string) {
-    // TODO: - Do something when the iframe loads.
-    console.log('load', event);
+  private subscribeToCreateItemService() {
+    this.createItemService.getLoadingChanged().subscribe((loading: boolean) => {
+      if (!loading) {
+        let response = this.createItemService.getServerResponse();
+        console.log('item id: ', response?.values?.item_id);
+      }
+    });
   }
 }
